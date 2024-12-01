4403b06023e4bd03513d75c9603eebb36abfe05f
utgwkk <utagawakiki@gmail.com>
2024-12-01 22:55:23 +0900

    get URL from query parameter

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,34 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
+const defaultErrorHeaders = {
+  "cache-control": "public, max-age=60, s-maxage=60",
+};
+
 app.get("/", (c) => {
-  return c.text("Hello Hono!");
+  const { url: urlFromQuery } = c.req.query();
+  if (!urlFromQuery) {
+    return c.text("url query parameter not specified", 400, {
+      ...defaultErrorHeaders,
+    });
+  }
+
+  let url: URL;
+  try {
+    url = new URL(urlFromQuery);
+  } catch (ex) {
+    return c.text(`invalid url: ${urlFromQuery}`, 400, {
+      ...defaultErrorHeaders,
+    });
+  }
+
+  if (!(url.protocol === "https:" || url.protocol === "http:")) {
+    return c.text("URL protocol must be either http or https", 400, {
+      ...defaultErrorHeaders,
+    });
+  }
+
+  return c.text(url.toString());
 });
 
 const port = Number(process.env.PORT || 3000);
